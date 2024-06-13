64a52ca7884166ed31c0b5e4124d846c0af11b1d
mkchoi <sh20151889@gmail.com>
2024-06-13 15:23:24 +0900

    20240613_crash

--- src/main/java/com/scraper/scraper/restApi/model/CoffeeBean.java
+++ src/main/java/com/scraper/scraper/restApi/model/CoffeeBean.java
@@ -1,7 +1,9 @@
 package com.scraper.scraper.restApi.model;
 
 import java.util.UUID;
+import jakarta.persistence.Entity;
 
+@Entity
 public class CoffeeBean {
     private final String id;
     private String name;
