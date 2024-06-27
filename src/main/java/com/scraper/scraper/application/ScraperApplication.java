db4db52776a596a890bc2fbd415df4f03f69abbb
mkchoi <sh20151889@gmail.com>
2024-06-27 14:22:21 +0900

    202406271422_dbconfig_remove

--- src/main/java/com/scraper/scraper/application/ScraperApplication.java
+++ src/main/java/com/scraper/scraper/application/ScraperApplication.java
@@ -2,9 +2,10 @@ package com.scraper.scraper.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = "com.scraper.scraper")
 public class ScraperApplication {
 
